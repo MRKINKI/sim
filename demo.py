ca6e9c56e196995a015e8d312f437b83ddc4e931
davic <davic@aawant.com>
2018-09-27 09:10:40 +0800

    change demo

--- demo.py
+++ demo.py
@@ -1,27 +1,79 @@
 # -*- coding: utf-8 -*-
 
 from main.tool import tokenize
-from main.prepro.tokenize_data import TokenizeData
 from main.dataset import Dataset
 from main.tool.ngram.ngram_transform import NgramTransform
+from main.inference import SemanticMatchingInference
 import configparser
+import json
+import pickle
+from time import time
 
 
 if __name__ == '__main__':
 
-    text = '人们通常只关注味道甜美的枇杷，很少会注意到其貌不扬的枇杷叶'
+    text = '花菇历来被国人作为延年益寿的补品，是香菇中的上品，含有丰富的营养价值，可帮助调节人体新陈代谢，助消化'
+    text = '冬天到了，吃什么比较好'
+    text = '天气冷了吃什么比较好'
+    text = '牙痛'
     conf = configparser.ConfigParser()
     conf.read("./conf.ini")
 
-    ds = Dataset(conf)
-    td = TokenizeData()
-    nt = NgramTransform()
-    tokenizer = tokenize.get_class('corenlp')()
-    # td.run(raw_file, prepro_file, tokenizer)
-    ds.build_tfidf()
-    # print(da.data)
+    af = json.loads(conf.get('key', 'all_field'))
 
-    words = tokenizer.segment(text)
-    ngram = nt.get_ngrams(words, n=2)
-    print(words)
-    print(ngram)
+    # ds = Dataset(conf)
+    # ds.segment_data()
+    # ds.ngram_data()
+    # ds.load_data(cat='segment')
+    # ds.build_tfidf_model()
+    # ds.build_lda_model()
+    # ds.build_fasttext_model()
+    # ds.build_word2vec_model()
+
+    # word = '鸡肉'
+    # model = pickle.load(open('./data/fasttext.model', 'rb'))
+    # model = pickle.load(open('./data/w2v.model', 'rb'))
+    # computer_vec = model.wv['甜']
+    # print(computer_vec)
+    # print(model.most_similar(word))
+
+    # lda_meta_data = pickle.load(open('./data/lda_metadata.data', 'rb'))
+    # # lda_index = lda_meta_data['illustration']['lda_index']
+    # dictionary = lda_meta_data['illustration']['dictionary']
+    # tokenizer = tokenize.get_class('corenlp')()
+    # words = tokenizer.segment(text)
+    # query_bow = [dictionary.doc2bow(words)]
+    # corpus_tfidf = lda_meta_data['illustration']['tfidf'][query_bow]
+    # lda_model = lda_meta_data['illustration']['lda_model']
+    # topics = lda_model.print_topics(num_topics=20, num_words=10)
+    # vector = lda_model[corpus_tfidf]
+    # print(list(vector))
+    #
+    # sims = lda_index[vector]
+    # print(sorted(enumerate(sims.squeeze()), key=lambda f: -f[1]))
+
+    # topic_terms = lda_meta_data['illustration']['lda_model'].get_topic_terms(15, topn=10)
+    #
+    # print(words)
+    # print(query_bow)
+    # print(list(vector))
+    # print([dictionary.id2token[t[0]] for t in topic_terms])
+
+    smi = SemanticMatchingInference(conf, 'topic')
+
+    # ngram = nt.get_ngrams(words, n=2)
+
+    # print(ngram)
+
+    # cands = smi.tfidf_rank_from_text(text, topk=3)
+    # print(cands)
+
+    # ["title", "efficacy", "illustration", "practice", "materials"]
+    field = 'illustration'
+    # text_id = '5ba3224507e6f00d78c4dca7'
+    #
+    t0 = time()
+    # cands = smi.tfidf_rank_from_id(text_id, field, topk=3)
+    cands = smi.topic_rank_from_text(text, field, topk=5)
+    print(time()-t0)
+    print(cands)
--- download_data.py
+++ download_data.py
@@ -8,14 +8,36 @@ class DownloadData:
         self.db = self.client['Food&Health']
         self.food_collection = self.db['meishij_tangzhou']
 
+    @staticmethod
+    def get_field_info(field, dic):
+        info = dic[field] if field in dic else ''
+        if isinstance(info, list):
+            return ' '.join(info)
+        else:
+            return info
+
+    @staticmethod
+    def get_material(dic):
+        materials = []
+        if '用料' not in dic:
+            return materials
+        # print(dic['_id'])
+        for k, sub_materials in dic['用料'].items():
+            for m in sub_materials:
+                materials.append(m['材料'])
+        return materials
+
     def download_all(self, output_file):
         with open(output_file, 'w', encoding='utf-8') as fout:
             for doc in self.food_collection.find():
-                sample = {'id': doc['_id'],
+                sample = {'id': str(doc['_id']),
                           'title': doc['title'],
-                          'efficacy': doc['功效'],
-                          'illustration': doc['简介说明']}
-                fout.write(json.loads(sample) + '\n')
+                          'efficacy': self.get_field_info('功效', doc),
+                          'illustration': self.get_field_info('简介说明', doc),
+                          'practice': self.get_field_info('做法', doc),
+                          'materials': self.get_material(doc)
+                          }
+                fout.write(json.dumps(sample) + '\n')
 
     def download_cho(self, output_file):
         with open(output_file, 'w', encoding='utf-8') as fout:
@@ -27,6 +49,7 @@ class DownloadData:
 
 
 if __name__ == '__main__':
-    file = './data/tangzhou.json'
+    file = './data/tangzhou_all.json'
     dd = DownloadData()
-    dd.download_cho(file)
+    dd.download_all(file)
+    # dd.download_cho(file)
